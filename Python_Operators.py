d0af1583a274444b2121f5f9be7ce3f2c49ec61e
shashank-ineuron <117538908+shashank-ineuron@users.noreply.github.com>
2022-11-06 17:17:43 +0530

    Update Python_Operators.py

--- Python_Operators.py
+++ Python_Operators.py
@@ -18,3 +18,23 @@ print("Float Division of x / y = ", x/y)
 print("Integer Divison of x // y = ", x//y)
 print("Modulus of x % y = ", x%y)
 print("Power of y on x i.e; x ** y = ", x**y)
+
+# concat operation for string
+full_name = str_data + " " + "mishra"
+print("Full name = ", full_name)
+
+# if we can use - as well ? IT will not work
+minus_str = "shashank" - "mishra"
+print("Minus str = ", minus_str)
+
+multiply_str = "shashank"*"mishra"
+print("Multiply str = ", multiply_str)
+
+power_str = "shashank"**"mishra"
+print("Power str = ", power_str)
+
+power_str = "shashank" ** 3
+print("Power str = ", power_str)
+
+multiply_numeric_str = "shashank"*5
+print("Multiply numeric str = ", multiply_numeric_str)
