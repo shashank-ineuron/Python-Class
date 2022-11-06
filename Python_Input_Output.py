d77a5aa9e635dd323e4ae234924d5b30117f0b30
shashank-ineuron <117538908+shashank-ineuron@users.noreply.github.com>
2022-11-06 16:52:58 +0530

    Update Python_Input_Output.py

--- Python_Input_Output.py
+++ Python_Input_Output.py
@@ -15,3 +15,40 @@ print("Value of int_var = ",int_var," - Result Done !!")
 print("Value of float_var = ",float_var," - Result Done !!")
 print("Value of string_var = ",string_var," - Result Done !!")
 print("Value of bool_var = ",bool_var," - Result Done !!")
+
+# How to check the data type of any variable in Python
+# We can use type() funtion
+print("Type of int_var ?", type(int_var))
+print("Type of float_var ?", type(float_var))
+print("Type of string_var ?", type(string_var))
+print("Type of bool_var ?", type(bool_var))
+
+# How to do the type casting in python ??
+# int() , float(), str(), bool()
+int_var = int_var + 10  # int_var = 10 + 10 and in next step int_var = 20
+casted_int_var = float(int_var)
+casted_float_var = int(float_var)
+
+print("Int to Float Typecasting for int_var = ",casted_int_var)
+print("Float to Int Typecasting for float_var = ",casted_float_var)
+
+numeric_str = "123"
+#numeric_str = numeric_str + 50 # is it valid ?? numeric_str = "123" + 50
+#print("Value of numeric_str = ",numeric_str)
+
+numeric_str = int(numeric_str) + 50 # numeric_str = int("123") + 50 -> 123+50 = 173
+print("Value of numeric_str = ",numeric_str)
+
+# How to take the inputs in Python ?
+# we can use input() function
+
+# name = input()
+# age = input()
+# print("User name = ",name)
+# print("User age = ",age)
+
+# Another way to take user input with custome message
+name = input("Enter value for name = ")
+age = input("Enter value for age = ")
+print("User name = ",name)
+print("User age = ",age)
