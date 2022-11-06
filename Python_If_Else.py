fdf4acbf1a7d31c26640defd2c0a839b242c6f87
shashank-ineuron <117538908+shashank-ineuron@users.noreply.github.com>
2022-11-06 18:19:56 +0530

    Update Python_If_Else.py

--- Python_If_Else.py
+++ Python_If_Else.py
@@ -21,3 +21,17 @@ if a==50:
     print("Yes, A is Equals to 50 !!")
 
 print("Bye !!")
+
+# Nested if-else condition
+marks = 54
+
+if marks>=90:
+    print("Grade A+")
+elif marks>=80 and marks<90:
+    print("Grade A")
+elif marks>=70 and marks<80:
+    print("Grade B+")
+elif marks>=60 and marks<70:
+    print("Grade B")
+else:
+    print("Grade C")
