3da64d9e8eb03a6aa450f0f35e3cc2db69ab9506
shashank-ineuron <117538908+shashank-ineuron@users.noreply.github.com>
2022-11-06 16:17:02 +0530

    Create Python_Input_Output.py

---
+++ Python_Input_Output.py
@@ -0,0 +1,17 @@
+# Declare & Assign Variables in python
+
+int_var = 10
+float_var = 18.25
+string_var = 'ineuron batch2' # or another way "ineuron batch2"
+bool_var = True # if we want to assign false then it should be like False
+
+
+# Function in python for Output
+# Function does what ? They might or minght not accept some parameter
+print("Hello World !!!!") 
+
+# Print variable values in python
+print("Value of int_var = ",int_var," - Result Done !!")
+print("Value of float_var = ",float_var," - Result Done !!")
+print("Value of string_var = ",string_var," - Result Done !!")
+print("Value of bool_var = ",bool_var," - Result Done !!")
