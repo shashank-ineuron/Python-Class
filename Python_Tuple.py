ae7742edb1664f8604a696fdc2d80e37dcb34be9
shashank-ineuron <117538908+shashank-ineuron@users.noreply.github.com>
2022-11-12 18:00:39 +0530

    Create Python_Tuple.py

---
+++ Python_Tuple.py
@@ -0,0 +1,20 @@
+# Work with tuple - Immutable object
+
+# empty tuple
+t1 = ()
+
+# failed assignment and append
+# t1.append(5)
+# t1[0] = 3
+
+t2 = (20,30,40,50)
+print(len(t2))
+
+# indexing also works for tuple
+# access 3rd element from tuple
+
+print(t2[2])
+
+# Indexing works
+# Slicing works
+# Iteration also works
