72e93881c0f3fd3293639cdd49192abdbf51709f
shashank-ineuron <117538908+shashank-ineuron@users.noreply.github.com>
2022-11-13 15:40:46 +0530

    Create Python_Dictionary.py

---
+++ Python_Dictionary.py
@@ -0,0 +1,37 @@
+# Dictionary in Python
+dict1 = {}
+print(type(dict1))
+
+# How to insert values in Dictionary
+dict2 = {}
+dict2['name'] = 'Shashank'
+dict2['age'] = 22
+dict2['skills'] = ['Python', 'Java']
+dict2['states_visited'] = ('UP', 'Goa')
+dict2[45] = 'Random Key'
+dict2['other_details'] = {'color' : 'Black', 'nationality' : 'Indian'}
+print(dict2)
+
+# Check the length of dictionary
+print(len(dict2))
+
+# how to access value of a particular key
+print(dict2['name'])
+print(dict2['skills'][0])
+print(dict2['other_details']['nationality'])
+
+
+
+# temp = dict2['other_details']
+# print(temp)
+
+
+
+
+
+# how to update value on a particular key
+
+
+
+
+
